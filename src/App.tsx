e20929840effc2ebafcb2439f9a3626cd4716fe1
virage81 <karimovminds@gmail.com>
2025-05-06 17:32:12 +0400

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+	return (
+		<div className='p-10 flex flex-col gap-10'>
+			<h1 className='text-4xl font-bold capitalize text-center'>Custom Hooks training</h1>
+			<div className='flex flex-col items-center justify-center gap-5'></div>
+		</div>
+	);
+}
